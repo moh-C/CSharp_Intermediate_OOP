1ef1df660e10a57f33002e11ba9d63660cc61cb3
Aaron <mohammad.chegini@hotmail.com>
2020-08-13 13:18:49 +0430

    Composition between classes

---
+++ Assosiation Between Classes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharp_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new DbMigrator(new Logger());
+            var install = new Install(new Logger());
+            db.Migrate();
+            install._Install();
+        }
+    }
+}
