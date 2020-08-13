38d0fa88703341dbe3a93a4f1794eb3f1326d143
Aaron <mohammad.chegini@hotmail.com>
2020-08-13 12:13:08 +0430

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
